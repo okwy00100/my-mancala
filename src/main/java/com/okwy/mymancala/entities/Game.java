09a630002aebef5d7b1b5976c6eaf6052a6f24fe
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-10-17 22:15:05 +0200

    Project entity structure ongoing

---
+++ src/main/java/com/okwy/mymancala/entities/Game.java
@@ -0,0 +1,10 @@
+package com.okwy.mymancala.entities;
+
+/**
+ * @author Okwy Nwachukwu
+ * created on 10/16/2022 - 3:54 PM | inside package com.okwy.mymancala.entities
+ * project name: my-mancala
+ */
+
+public class Game {
+}
