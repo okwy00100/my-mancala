d88630a9b4c42f8aa91d15a50a2eb3b1db177a15
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-10-25 23:10:03 +0200

    Project entity structure ongoing - variable type fix in player class

--- src/main/java/com/okwy/mymancala/entities/Player.java
+++ src/main/java/com/okwy/mymancala/entities/Player.java
@@ -1,14 +1,16 @@
 package com.okwy.mymancala.entities;
 
+
 /**
  * @author Okwy Nwachukwu
  * created on 10/16/2022 - 3:55 PM | inside package com.okwy.mymancala.entities
  * project name: my-mancala
  */
 
+
 public class Player {
 
-    private String playerId;
+    private Long playerId;
 
     private String playerName;
 
