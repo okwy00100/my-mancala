e73524504eacf8246fadc6f685b858359dadd6f6
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-11-01 07:17:52 +0100

    Project structure ongoing - added enums

---
+++ src/main/java/com/okwy/mymancala/enums/GameState.java
@@ -0,0 +1,10 @@
+package com.okwy.mymancala.enums;
+
+/**
+ * @author Okwy Nwachukwu
+ * created on 11/1/2022 - 7:15 AM | inside package com.okwy.mymancala.enums
+ * project name: my-mancala
+ */
+
+public enum GameState {
+}
---
+++ src/main/java/com/okwy/mymancala/enums/GameType.java
@@ -0,0 +1,10 @@
+package com.okwy.mymancala.enums;
+
+/**
+ * @author Okwy Nwachukwu
+ * created on 11/1/2022 - 7:16 AM | inside package com.okwy.mymancala.enums
+ * project name: my-mancala
+ */
+
+public enum GameType {
+}
