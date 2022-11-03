df07e000366c8fb18b2eee6cbd8f4d86e183f84f
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-11-03 14:40:56 +0100

    Entity Implementation - Game entity setup ongoing

--- src/main/java/com/okwy/mymancala/entities/Game.java
+++ src/main/java/com/okwy/mymancala/entities/Game.java
@@ -1,5 +1,9 @@
 package com.okwy.mymancala.entities;
 
+import com.okwy.mymancala.enums.GameResult;
+import com.okwy.mymancala.enums.GameState;
+import com.okwy.mymancala.enums.GameType;
+
 /**
  * @author Okwy Nwachukwu
  * created on 10/16/2022 - 3:54 PM | inside package com.okwy.mymancala.entities
@@ -10,5 +14,11 @@ public class Game {
 
     private String gameId;
 
+    private GameType gameType;
+
+    private GameState gamestate;
+
     private Player winner;
+
+    private GameResult gameResult;
 }
