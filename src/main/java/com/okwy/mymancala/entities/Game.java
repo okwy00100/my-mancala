1f2651fd9d1b7c80db7bb5e49d07e3dc36692938
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-11-06 10:29:20 +0100

    Entity Implementation - Game entity setup complete

--- src/main/java/com/okwy/mymancala/entities/Game.java
+++ src/main/java/com/okwy/mymancala/entities/Game.java
@@ -3,6 +3,11 @@ package com.okwy.mymancala.entities;
 import com.okwy.mymancala.enums.GameResult;
 import com.okwy.mymancala.enums.GameState;
 import com.okwy.mymancala.enums.GameType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 /**
  * @author Okwy Nwachukwu
@@ -10,16 +15,28 @@ import com.okwy.mymancala.enums.GameType;
  * project name: my-mancala
  */
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "game")
 public class Game {
 
+    @Id
+    @Column(name = "game_id")
     private String gameId;
 
+    @Column(name = "game_type")
     private GameType gameType;
 
+    @Column(name = "state", nullable = false)
     private GameState state;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "winner_player_id")
     private Player winner;
 
+    @Column(name = "result")
     private GameResult gameResult;
 
 
