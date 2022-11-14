c11beee41c8ba71c5fb5afa86f88546feb8d4bcb
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-11-14 21:18:38 +0100

    Entity Implementation - Mancala entity setup ongoing

---
+++ src/main/java/com/okwy/mymancala/entities/mancala/Mancala.java
@@ -0,0 +1,16 @@
+package com.okwy.mymancala.entities.mancala;
+
+import com.okwy.mymancala.entities.Game;
+
+/**
+ * @author Okwy Nwachukwu
+ * created on 11/14/2022 - 9:16 PM | inside package com.okwy.mymancala.entities.mancala
+ * project name: my-mancala
+ */
+
+public class Mancala {
+
+    private String id;
+
+    private Game game;
+}
