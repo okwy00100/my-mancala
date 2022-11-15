171233dea12ec033b621d13d537ee499b638450c
Okwy Nwachukwu <okwy.m.nwachukwu@gmail.com>
2022-11-15 15:42:27 +0100

    Entity Implementation - Mancala entity setup ongoing: added pit and pitrow

---
+++ src/main/java/com/okwy/mymancala/entities/mancala/PitRow.java
@@ -0,0 +1,10 @@
+package com.okwy.mymancala.entities.mancala;
+
+/**
+ * @author Okwy Nwachukwu
+ * created on 11/15/2022 - 3:41 PM | inside package com.okwy.mymancala.entities.mancala
+ * project name: my-mancala
+ */
+
+public class PitRow {
+}
